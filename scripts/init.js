cb991d47f931656ccababb483510a78e3b7dd4ae
Dimka122 <kassindima1@gmail.com>
2025-10-12 11:41:47 +0300

    пока так

--- scripts/init.js
+++ scripts/init.js
@@ -1,13 +1,7 @@
 $(document).ready(function() {
     // Инициализация круговых диаграмм
     if($.fn.circliful) {
-        $('#test-circle1').circliful();
-        $('#test-circle2').circliful();
-        $('#test-circle3').circliful();
-        $('#test-circle4').circliful();
-        $('#test-circle5').circliful();
-        $('#test-circle6').circliful();
-        $('#test-circle7').circliful();
-        $('#test-circle8').circliful();
+        $('.circliful-chart').circliful();
+        
     }
 });
