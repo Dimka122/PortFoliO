b076132b61905f48a3adc1794e97a7d0eb7011e0
Dimka122 <kassindima1@gmail.com>
2025-10-12 11:31:58 +0300

    init.js

---
+++ scripts/init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Инициализация круговых диаграмм
+    if($.fn.circliful) {
+        $('#test-circle1').circliful();
+        $('#test-circle2').circliful();
+        $('#test-circle3').circliful();
+        $('#test-circle4').circliful();
+        $('#test-circle5').circliful();
+        $('#test-circle6').circliful();
+        $('#test-circle7').circliful();
+        $('#test-circle8').circliful();
+    }
+});
